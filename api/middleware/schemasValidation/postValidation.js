395ab757149816a7c9eb8b7730c0309c33e0e307
NYIRIGIRA SERGE <nyirigira@NYIRIGIRAs-MacBook-Air.local>
2023-01-21 20:09:57 +0200

    Add: Add of image property in models

--- api/middleware/schemasValidation/postValidation.js
+++ api/middleware/schemasValidation/postValidation.js
@@ -1,19 +1,11 @@
-import Joi from 'joi'
+import Joi from "joi";
 
 const postValidation = Joi.object({
-  title: Joi.string().trim()
-    .min(3)
-    .max(30)
-    .required(),
-
-  content: Joi.string().trim()
-    .min(3)
-    .max(1500)
-    .required()
-
-})
-  .with('title', 'content')
+  cover: Joi.string().trim(),
+  title: Joi.string().trim().min(3).max(30).required(),
+  content: Joi.string().trim().min(3).max(1500).required(),
+}).with("title", "content");
 
 export default {
-  postValidation
-}
+  postValidation,
+};
